2d14e72a9e70a4d6f489258393b42216b894c67f
Eric Liang <eliang7@wisc.edu>
2023-10-21 02:22:44 +0000

    cs400 script submission from Fri Oct 20 21:22:44 CDT 2023

--- IterableMultiKeyRBT.java
+++ IterableMultiKeyRBT.java
@@ -1,9 +1,22 @@
+// --== CS400 Fall 2023 File Header Information ==--
+// Name: <Eric Liang>
+// Email: <eliang7@wisc.edu>
+// Group: <C17>
+// TA: <Matthew Schwennesen>
+// Lecturer: <Florian>
+// Notes to Grader: <optional extra notes>
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class IterableMultiKeyRBT<T extends Comparable<T>> extends RedBlackTree<KeyListInterface<T>> 
 implements IterableMultiKeySortedCollectionInterface<T>, Iterable<T>{
+
+    private Comparable<T> iterationStartPoint; // Field to store the iteration start point
+    private int numberOfKeys; // Field to store the number of keys in the tree
 
     /**
      * Inserts value into tree that can store multiple objects per key by keeping
@@ -14,9 +27,20 @@ implements IterableMultiKeySortedCollectionInterface<T>, Iterable<T>{
     @Override
     public boolean insertSingleKey(T key) {
         // TODO Auto-generated method stub
-    
-        return false;
-        
+        KeyListInterface<T> newKeyList = new KeyList<>(key);
+        Node<KeyListInterface<T>> existingNode = findNode(newKeyList);
+
+        if(existingNode != null){
+            existingNode.data.addKey(key);
+            numberOfKeys++;
+            return false; 
+
+        } else {
+            super.insert(newKeyList);
+            numberOfKeys++;
+            return true;
+        }
+                
     }
 
     /**
@@ -25,7 +49,7 @@ implements IterableMultiKeySortedCollectionInterface<T>, Iterable<T>{
     @Override
     public int numKeys() {
         // TODO Auto-generated method stub
-        return 0;
+        return numberOfKeys;
     }
 
     /**
@@ -33,8 +57,54 @@ implements IterableMultiKeySortedCollectionInterface<T>, Iterable<T>{
      */
     @Override
     public Iterator<T> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+        return new Iterator<T>() {
+            Stack<RBTNode<KeyListInterface<T>>> start = getStartStack();
+            Iterator<T> iterator = null;
+            T next = null;
+    
+            @Override
+            public boolean hasNext() {
+                if (next != null) {
+                    return true;
+                }
+    
+                while ((iterator == null || !iterator.hasNext()) && !start.isEmpty()) {
+                    RBTNode<KeyListInterface<T>> node = start.pop();
+                    iterator = node.data.iterator();
+    
+                    // Move to the right-most child of the left subtree (if exists)
+                    if (node.down[1] != null) {
+                        RBTNode<KeyListInterface<T>> current = (RBTNode<KeyListInterface<T>>) node.down[1];
+                        while (current != null) {
+                            start.push(current);
+                            current = (RBTNode<KeyListInterface<T>>) current.down[0];
+                        }
+                    }
+                }
+    
+                if (iterator != null && iterator.hasNext()) {
+                    next = iterator.next();
+                    return true;
+                }
+    
+
+                return false;
+            }
+    
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+    
+
+                T keyToReturn = next;
+                next = null;
+                return keyToReturn;
+
+
+            }
+        };
     }
 
     /**
@@ -45,8 +115,48 @@ implements IterableMultiKeySortedCollectionInterface<T>, Iterable<T>{
      */
     @Override
     public void setIterationStartPoint(Comparable<T> startPoint) {
-        // TODO Auto-generated method stub
+        this.iterationStartPoint = startPoint;
     }
+
+    /**
+     * Removes all keys from the tree.
+     */
+    @Override
+     public void clear() {
+        super.clear();
+        
+        numberOfKeys = 0;
+
+    }
+
+    protected Stack<RBTNode<KeyListInterface<T>>> getStartStack() {
+       
+        Stack<RBTNode<KeyListInterface<T>>> stack = new Stack<>();
+       RBTNode<KeyListInterface<T>> currentKey = (RBTNode<KeyListInterface<T>>) this.root;
+
+       while (currentKey != null) {
+        Iterator<T> iterator = currentKey.data.iterator();
+        T firstKey = iterator.hasNext() ? iterator.next() : null;
+
+        if (firstKey != null && (iterationStartPoint == null || iterationStartPoint.compareTo(firstKey) <= 0)) {
+            stack.push(currentKey);
+        }
+
+        if (iterationStartPoint == null || (firstKey != null && iterationStartPoint.compareTo(firstKey) <= 0)) {
+            currentKey = (RBTNode<KeyListInterface<T>>) currentKey.down[0];
+        } else {
+            currentKey = (RBTNode<KeyListInterface<T>>) currentKey.down[1];
+        }
+    }
+
+    return stack;
+}
+
+
+
+
+
+
 
 
     //testers
@@ -86,20 +196,28 @@ implements IterableMultiKeySortedCollectionInterface<T>, Iterable<T>{
     @Test
     public void testSetIterationStartPoint() {
         IterableMultiKeyRBT<Double> tree = new IterableMultiKeyRBT<>();
-        tree.insertSingleKey(1.1);
-        tree.insertSingleKey(2.2);
-        tree.insertSingleKey(3.3);
+    tree.insertSingleKey(1.1);
+    tree.insertSingleKey(2.2);
+    tree.insertSingleKey(3.3);
 
-        tree.setIterationStartPoint(2.0);
-        Iterator<Double> iterator = tree.iterator();
-        Assertions.assertTrue(iterator.hasNext());
-        Assertions.assertEquals(2.2, iterator.next());
-        Assertions.assertTrue(iterator.hasNext());
-        Assertions.assertEquals(3.3, iterator.next());
-        Assertions.assertFalse(iterator.hasNext());
+    tree.setIterationStartPoint(2.0);
 
-        tree.setIterationStartPoint(4.0); // Setting a start point beyond the keys in the tree
-        iterator = tree.iterator();
-        Assertions.assertFalse(iterator.hasNext());
+    // Initialize iterator after setting the iteration start point
+    Iterator<Double> iterator = tree.iterator();
+
+    Assertions.assertTrue(iterator.hasNext());
+    Assertions.assertEquals(2.2, iterator.next());
+    Assertions.assertTrue(iterator.hasNext());
+    Assertions.assertEquals(3.3, iterator.next());
+    Assertions.assertFalse(iterator.hasNext());
+
+    tree.setIterationStartPoint(4.0);
+
+    // Initialize iterator again after changing the start point
+    iterator = tree.iterator();
+
+    Assertions.assertFalse(iterator.hasNext());
     }
+       
+    
 }
